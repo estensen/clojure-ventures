40c342042c06257cc5bd3071e32c8d13c7b70e95
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-16 10:15:08 +0200

    Add functions

--- src/learnclojure.clj
+++ src/learnclojure.clj
@@ -3,3 +3,15 @@
 (println (+ 7654 1234))
 
 (println (/ (* (+ 7 3) (+ 4 5)) 10))
+
+(defn hello-world [] "Hello world")
+(println (hello-world))
+
+(defn greet [name] (str "Hello, " name))
+(println (greet "Borat"))
+
+(defn hello2
+  ([] "Hello")
+  ([name] (str "Hello, " name)))
+(println (hello2))
+(println (hello2 "Håvard"))
